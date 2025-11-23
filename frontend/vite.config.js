58e03f946da7799c8632e26f5f1b5119dd511d44
Yogita <yogita.senthil@athenatec.com>
2025-11-23 12:22:00 -0800

    adding files for initial vercel deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/',
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+  },
   server: {
     port: 3000,
     open: true
